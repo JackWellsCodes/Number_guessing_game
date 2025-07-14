1e62969b9cd168495f7dddf82c47a30b33654545
JackWellsCodes <jackwellsapp@gmail.com>
2025-07-14 09:12:31 +0100

    Added in user input and conditionals

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,3 +1,14 @@
 import random
 
 number = random.randint(1, 100)
+
+guess = int(input("what's your guess? "))
+
+if guess == number:
+    print("Correct")
+elif guess < number:
+    print("Too low")
+else: 
+    print("Too high")
+
+    
