451208681ca25876b3f7ec64aa805968bf59b20a
JackWellsCodes <jackwellsapp@gmail.com>
2025-07-14 09:39:24 +0100

    while statement introduced

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,13 +2,20 @@ import random
 
 number = random.randint(1, 100)
 
-guess = int(input("what's your guess? "))
 
-if guess == number:
-    print("Correct")
-elif guess < number:
-    print("Too low")
-else: 
-    print("Too high")
+
+while True:
+    try:
+        guess = int(input("what's your guess? "))
+
+        if guess == number:
+            print("Correct")
+            break
+        elif guess < number:
+             print("Too low")
+        else: 
+            print("Too high")
+    except ValueError:
+        print("Please enter a valid integer.")
 
     
